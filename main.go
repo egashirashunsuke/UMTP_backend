0afea80355f00930db2275307eab07750c666e80
江頭俊亮 <egashirashunsuke@egashirashunsukenoMacBook-Air.local>
2025-05-16 15:18:25 +0900

    openai_setup

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
 )
-
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Worl!")
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal(".env ファイルの読み込みに失敗しました")
+		}
+
+		apiKey := os.Getenv("OPENAI_API_KEY")
+		if apiKey == "" {
+			log.Fatal("APIキーが環境変数に設定されていません")
+		}
+		client := openai.NewClient(
+			option.WithAPIKey(apiKey),
+		)
+		chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage("日本語に対応していますか？"),
+			},
+			Model: openai.ChatModelGPT4o,
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+		println(chatCompletion.Choices[0].Message.Content)
+
+		return c.String(http.StatusOK, chatCompletion.Choices[0].Message.Content)
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
