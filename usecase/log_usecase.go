1d6efdc01d344f00587928f0e1647b26228d5c9c
江頭俊亮 <egashirashunsuke@egashirashunsukenoMacBook-Air.local>
2025-09-26 17:13:46 +0900

    fix:ログのスキーマ変更

--- usecase/log_usecase.go
+++ usecase/log_usecase.go
@@ -1,17 +1,23 @@
 package usecase
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/egashirashunsuke/UMTP_backend/model"
 	"github.com/egashirashunsuke/UMTP_backend/repository"
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 )
 
 type LogData struct {
-	DeviceId  string `json:"device_id"`
-	EventName string `json:"event_name"`
-	Timestamp string `json:"timestamp"`
+	DeviceId       string             `json:"device_id"`
+	QuestionId     int                `json:"question_id"`
+	EventName      string             `json:"event_name"`
+	Answers        map[string]*string `json:"answers"`
+	HintOpenStatus map[string]bool    `json:"hint_open_status"`
+	Hints          map[string]*string `json:"hints"`
+	Timestamp      string             `json:"timestamp"`
 }
 
 type ILogUsecase interface {
@@ -31,11 +37,23 @@ func (lu *LogUsecase) SendLog(logdata *LogData) error {
 	if err != nil {
 		return err
 	}
+	detailsMap := map[string]interface{}{
+		"answers":          logdata.Answers,
+		"hint_open_status": logdata.HintOpenStatus,
+		"hints":            logdata.Hints,
+	}
+
+	detailsJSON, err := json.Marshal(detailsMap)
+	if err != nil {
+		return err
+	}
 
 	op := &model.OperationLog{
 		DeviceId:        devUUID,
+		QuestionId:      logdata.QuestionId,
 		EventName:       logdata.EventName,
 		Timestamp:       logdata.Timestamp,
+		Details:         datatypes.JSON(detailsJSON),
 		ServerTimestamp: time.Now(),
 	}
 
