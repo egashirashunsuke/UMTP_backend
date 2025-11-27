9b154e62b392a8b11ec7f9a1da57f327e9d05212
江頭俊亮 <egashirashunsuke@egashirashunsukenoMacBook-Air.local>
2025-11-27 11:43:27 +0900

    fix:プロンプトを調整

--- model/hint.go
+++ model/hint.go
@@ -4,7 +4,6 @@ type Hint struct {
 	ID           int      `json:"id" gorm:"primaryKey"`
 	QuestionID   uint     `json:"question_id"`
 	Question     Question `json:"question" gorm:"foreignKey:QuestionID"`
-	AnswersState string   `json:"answers_state" gorm:"uniqueIndex:idx_question_state"`
-	Hints        string   `json:"hints" gorm:"type:text"` // JSON配列として保存
-	CreatedAt    int64    `gorm:"autoCreateTime"`
+	AnswersState string   `json:"answers_state"`
+	Hints        string   `json:"hints" gorm:"type:text"`
 }
